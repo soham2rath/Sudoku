01d564f88a84514786986fe005f34cc5f5b1c2ef
soham2rath <sohamrath710@gmail.com>
2025-04-17 19:05:22 +0200

    Create SudokuGenerator.py

---
+++ SudokuGenerator.py
@@ -0,0 +1,20 @@
+from sudoku import Sudoku
+
+puzzle = Sudoku(3).difficulty(0.5)
+#puzzle.show()
+
+solution = puzzle.solve()
+#solution.show()
+
+solution = solution.board
+puzzle = puzzle.board
+
+for i in range(len(puzzle)):
+    for n in range(len(puzzle[i])):
+        if puzzle[i][n] == None:
+            puzzle[i][n] = 0
+
+for i in range(len(solution)):
+    for n in range(len(solution[i])):
+        if solution[i][n] == None:
+            solution[i][n] = 0
